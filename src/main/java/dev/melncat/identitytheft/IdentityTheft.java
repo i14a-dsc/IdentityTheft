d5fdc36eae38979d90cb30253f9767d9bdafdab3
I14A <dev.i14a.dsc@gmail.com>
2026-06-30 19:24:11 +0900

    chore: fix register command

--- src/main/java/dev/melncat/identitytheft/IdentityTheft.java
+++ src/main/java/dev/melncat/identitytheft/IdentityTheft.java
@@ -3,11 +3,16 @@ package dev.melncat.identitytheft;
 import dev.melncat.identitytheft.command.ExitCommand;
 import dev.melncat.identitytheft.command.IdentityTheftCommand;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 
 public final class IdentityTheft extends JavaPlugin {
     private IdentityTheftConfig config;
-    
+
     public IdentityTheftConfig getITConfig() {
         return config;
     }
@@ -16,17 +21,35 @@ public final class IdentityTheft extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
         config = new IdentityTheftConfig(getConfig());
+
         IdentityTheftCommand command = new IdentityTheftCommand(this);
-        getCommand("identitytheft")
-            .setExecutor(command);
-        getCommand("identitytheft")
-            .setTabCompleter(command);
+        registerCommand("identitytheft", command, command, Arrays.asList("it"), "The main command.", "identitytheft.command.identitytheft");
 
         ExitCommand exitCommand = new ExitCommand(this);
-        getCommand("exit")
-            .setExecutor(exitCommand);
+        registerCommand("exit", exitCommand, null, null, "Resets your identity without permission checks.", null);
 
         Bukkit.getPluginManager().registerEvents(new IdentityTheftListener(this), this);
+    }
+
+    private void registerCommand(String name, org.bukkit.command.CommandExecutor executor,
+                                  org.bukkit.command.TabCompleter tabCompleter,
+                                  java.util.List<String> aliases, String description, String permission) {
+        try {
+            Constructor<PluginCommand> constructor = PluginCommand.class.getDeclaredConstructor(String.class, org.bukkit.plugin.Plugin.class);
+            constructor.setAccessible(true);
+            PluginCommand cmd = constructor.newInstance(name, this);
+            cmd.setExecutor(executor);
+            if (tabCompleter != null) cmd.setTabCompleter(tabCompleter);
+            if (aliases != null) cmd.setAliases(aliases);
+            if (description != null) cmd.setDescription(description);
+            if (permission != null) cmd.setPermission(permission);
+
+            CommandMap commandMap = getServer().getCommandMap();
+            commandMap.register(getPluginMeta().getName(), cmd);
+        } catch (Exception e) {
+            getLogger().severe("Failed to register command: " + name);
+            e.printStackTrace();
+        }
     }
     
 
